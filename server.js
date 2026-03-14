cae62a727715995afb6a0c8a63cdec2bca1ac7a5
thara1999 <tamilkrishnan20@gmail.com>
2026-03-14 18:34:54 +0530

    apk2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const { initSocket } = require("./sockets/socket");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 /* =========================
    DATABASE CONNECTION
